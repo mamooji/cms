8ef46c5806092b8414501d4877ffc221d2bb09dc
mamooji <mamoojim@hotmail.com>
2024-03-19 17:34:34 -0400

    navigation menu work

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,29 +7,75 @@ import {
 } from "@/components/ui/navigation-menu";
 
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import ThemeToggle from "./ThemeToggle";
+import { navData } from "@/lib/content/navItems";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "./ui/button";
+import { Icons } from "./icons";
 
 const Navbar = () => {
+  const [sideBar, setSideBar] = useState(false);
+
   return (
-    <div className="flex w-full mx-auto max-w-3xl justify-between m-4">
+    <div className="flex w-full mx-auto justify-between m-4 ">
       <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/about" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                About
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+        <NavigationMenuList className=" md:hidden">
+          <Sheet open={sideBar} onOpenChange={setSideBar}>
+            <Button
+              variant="outline"
+              onClick={() => setSideBar(!sideBar)}
+              size={"icon"}
+            >
+              <Icons.hamburger />
+            </Button>
+            <SheetContent side={"left"}>
+              <SheetHeader></SheetHeader>
+              <NavigationMenuList className="flex flex-col">
+                {navData.map((navItem, index) => {
+                  return (
+                    <NavigationMenuItem key={index}>
+                      <Link
+                        href={navItem.url}
+                        legacyBehavior
+                        type="submit"
+                        passHref
+                      >
+                        <NavigationMenuLink
+                          onClick={() => setSideBar(false)}
+                          className={navigationMenuTriggerStyle()}
+                        >
+                          {navItem.name}
+                        </NavigationMenuLink>
+                      </Link>
+                    </NavigationMenuItem>
+                  );
+                })}
+              </NavigationMenuList>
+            </SheetContent>
+          </Sheet>
+        </NavigationMenuList>
+        <NavigationMenuList className="hidden md:flex">
+          {navData.map((navItem, index) => {
+            return (
+              <NavigationMenuItem key={index}>
+                <Link href={navItem.url} legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    {navItem.name}
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            );
+          })}
         </NavigationMenuList>
       </NavigationMenu>
       <NavigationMenu>
---
+++ src/lib/content/navItems.ts
@@ -0,0 +1,39 @@
+interface NavItem {
+  name: string;
+  url: string;
+  logo?: SVGAElement;
+}
+export const navData: Array<NavItem> = [
+  {
+    name: "home",
+    url: "/",
+  },
+  {
+    name: "about us",
+    url: "/about",
+  },
+  {
+    name: "prayer times",
+    url: "/prayer-times",
+  },
+  {
+    name: "services",
+    url: "/services",
+  },
+  {
+    name: "events",
+    url: "/events",
+  },
+  {
+    name: "donate",
+    url: "/donate",
+  },
+  {
+    name: "members",
+    url: "/members",
+  },
+  {
+    name: "contact",
+    url: "/contact",
+  },
+];
